00f32018b8c061baf2376319b8bf643beef87dd5
Suguru Kato <salmon.bioinformatics@gmail.com>
2016-09-23 18:21:00 +0900

    Add memory.h

--- src/container.c
+++ src/container.c
@@ -1,12 +1,6 @@
 #include <stdlib.h>
+#include "memory.h"
 #include "container.h"
-
-inline float* alloc_float(const int n) {
-    float* heap = (float*)malloc(sizeof(float) * n);
-    if (heap == NULL)
-        exit(1); // TODO Error Message
-    return heap;
-}
 
 void alloc_cont(const int num_beads, Container* container) {
     container->num_beads = num_beads;
---
+++ src/memory.h
@@ -0,0 +1,13 @@
+#ifndef __MINIMD_MEMORY_H
+#define __MINIMD_MEMORY_H
+
+#include <stdlib.h>
+
+inline float* alloc_float(const int n) {
+    float* heap = (float*)malloc(sizeof(float) * n);
+    if (heap == NULL)
+        exit(1); // TODO Error Message
+    return heap;
+}
+
+#endif

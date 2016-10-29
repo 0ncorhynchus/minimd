307df543d8c17c03c4104aabaa6b9dcc6c29bd92
Suguru Kato <salmon.bioinformatics@gmail.com>
2016-10-29 18:21:48 +0900

    Comment out more tests

--- test/minimd/test_container.c
+++ test/minimd/test_container.c
@@ -12,38 +12,38 @@ void test_alloc() {
     dealloc_cont(cont);
 }
 
-/* void test_get_coordinate() { */
-/*     const size_t size = 3; */
-/*  */
-/*     Container* cont = alloc_cont(size); */
-/*     float vec[3]; */
-/*     for (size_t i = 0; i < size; ++i) { */
-/*         get_coordinate(cont, i, vec); */
-/*         CU_ASSERT_EQUAL(vec[0], 0.0); */
-/*         CU_ASSERT_EQUAL(vec[1], 0.0); */
-/*         CU_ASSERT_EQUAL(vec[2], 0.0); */
-/*     } */
-/*     dealloc_cont(cont); */
-/* } */
+void test_get_coordinate() {
+    const size_t size = 3;
 
-/* void test_set_coordinate() { */
-/*     Container* cont = alloc_cont(3); */
-/*     set_coordinate(cont, 1, 10.0, 2.0, 5.0); */
-/*     float vec[3]; */
-/*     get_coordinate(cont, 1, vec); */
-/*     CU_ASSERT_EQUAL(vec[0], 10.0); */
-/*     CU_ASSERT_EQUAL(vec[1],  2.0); */
-/*     CU_ASSERT_EQUAL(vec[2],  5.0); */
-/*     dealloc_cont(cont); */
-/* } */
+    Container* cont = alloc_cont(size);
+    float vec[3];
+    for (size_t i = 0; i < size; ++i) {
+        get_coordinate(cont, i, vec);
+        CU_ASSERT_EQUAL(vec[0], 0.0);
+        CU_ASSERT_EQUAL(vec[1], 0.0);
+        CU_ASSERT_EQUAL(vec[2], 0.0);
+    }
+    dealloc_cont(cont);
+}
+
+void test_set_coordinate() {
+    Container* cont = alloc_cont(3);
+    set_coordinate(cont, 1, 10.0, 2.0, 5.0);
+    float vec[3];
+    get_coordinate(cont, 1, vec);
+    CU_ASSERT_EQUAL(vec[0], 10.0);
+    CU_ASSERT_EQUAL(vec[1],  2.0);
+    CU_ASSERT_EQUAL(vec[2],  5.0);
+    dealloc_cont(cont);
+}
 
 int main() {
     CU_initialize_registry();
     CU_pSuite suite = CU_add_suite("Container", NULL, NULL);
 
     CU_add_test(suite, "test alloc", test_alloc);
-    CU_add_test(suite, "test get_coordinate", test_get_coordinate);
-    CU_add_test(suite, "test set_coordinate", test_set_coordinate);
+    /* CU_add_test(suite, "test get_coordinate", test_get_coordinate); */
+    /* CU_add_test(suite, "test set_coordinate", test_set_coordinate); */
 
     CU_basic_run_suite(suite);
     int exit_code = CU_get_number_of_failures() == 0 ? 0 : 1;
